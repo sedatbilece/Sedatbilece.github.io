565e2e342212a800d4cee17802c4368b82ef0607
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-04-06 09:59:27 +0300

    son düzenlemeler

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const panels=document.querySelectorAll(".panel");// return to list 5 panel
+const panels=document.querySelectorAll(".panel");// tüm panellerin listesini döndürür
 
 
-panels.forEach((panel)=>{// step on 5 panel 
+panels.forEach((panel)=>{//paneller içinde geziniyoruz
 
-panel.addEventListener("click",()=>{
-     removeActiveClasses();
-     panel.classList.add("active");
+panel.addEventListener("click",()=>{//tüm panellere click event geldiğinde 
+     removeActiveClasses();// aktif panel hangisi olursa olsun aktifliği kaldırılıp
+     panel.classList.add("active");//tıklanan panele aktif ekleniyor
 })
 
 
 })
 
-function  removeActiveClasses(){
+function  removeActiveClasses(){//aktiflik kaldırma fonksiyonumuz
 
     panels.forEach( panel=>{
         panel.classList.remove("active");
