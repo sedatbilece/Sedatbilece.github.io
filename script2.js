356e2e3106b24b4fad9ebc6626083ae28d70811e
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-04-08 11:06:25 +0300

    açıklamalar

--- script2.js
+++ script2.js
@@ -1,31 +1,34 @@
-const buton=document.querySelector("#modebut");
-const boxs=document.querySelectorAll('.box'); 
-const eltitle =document.querySelectorAll('.eltitle');
-const eltitlea=document.querySelectorAll(".eltitlea");
-buton.addEventListener("click",function(){
+const buton=document.querySelector("#modebut");//tek eleman
+const boxs=document.querySelectorAll('.box'); //Liste
+const eltitle =document.querySelectorAll('.eltitle');//Liste
+const eltitlea=document.querySelectorAll(".eltitlea");//Liste
 
 
 
-boxs.forEach((box)=>{
-
-    box.classList.toggle('box');
-    box.classList.toggle('box2');
-});
+buton.addEventListener("click",function(){// Butona tıklanırsa
 
 
-eltitle.forEach((tit)=>{
+
+boxs.forEach((box)=>{//tüm boxlar üzerinde gezip 
+
+    box.classList.toggle('box');// box toggle (dark)    0 1
+    box.classList.toggle('box2');// box2 toggle (light) 1 0
+});//boxs
+
+
+eltitle.forEach((tit)=>{//box ile aynı işlemler
 
     tit.classList.toggle('eltitle');
     tit.classList.toggle('eltitle2');
-});
+});//eltitle
 
-eltitlea.forEach((tit)=>{
+eltitlea.forEach((tit)=>{//box ile aynı işlemler
 
     tit.classList.toggle('eltitlea');
     tit.classList.toggle('eltitlea2');
-});
+});//eltitlea
 
 
 
 
-});
+});//buton click event end
